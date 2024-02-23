5b2da067f1f1efaf25362eece17ed489b5717003
Khue Dao <you@example.com>
2024-02-23 22:57:54 +0700

    làm xog cái navigation bar òi ye ye
    như tiêu đề

--- App.js
+++ App.js
@@ -1,35 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { mapping, light as lightTheme } from '@eva-design/eva'
-import { ApplicationProvider } from 'react-native-ui-kitten'
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider,IconRegistry, Layout, Text  } from '@ui-kitten/components'
+import { EvaIconsPack } from '@ui-kitten/eva-icons'
 
 import Login from './assets/login/login';
 import Regist from './assets/regist/registration';
-import tabNavigator from './assets/tabnavigator/tab-navigator';
+import MyTabs from './assets/tabnavigator/tab-navigator';
 
 const Stack = createNativeStackNavigator();
 
-
-
-export default function App() {
-  return (
-    <ApplicationProvider mapping={mapping} theme={lightTheme}>
-        <TabNavigator />
-    </ApplicationProvider>
-  );
+export default () => (
   
-}
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  <NavigationContainer>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      < ApplicationProvider {...eva} theme={eva.light}>
+          <MyTabs />
+      </ApplicationProvider>
+    </>
+  </NavigationContainer>
+);
---
+++ assets/tabnavigator/screen/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, Layout } from '@ui-kitten/components'
+
+const Dashboard = () => (
+  <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Dashboard</Text>
+  </Layout>
+)
+
+export default Dashboard
