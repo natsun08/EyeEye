7fb1dcba55003777e5d1497b4782d40523b0040d
Khue Dao <you@example.com>
2024-02-23 22:59:04 +0700

    vẫn là làm xog  navigation bar nhưng nãy lỡ xóa 1 page...
    như tên

---
+++ assets/tabnavigator/screen/leaderboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, Layout } from '@ui-kitten/components'
+
+const Leaderboard = () => (
+  <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Leaderboard</Text>
+  </Layout>
+)
+
+export default Leaderboard
