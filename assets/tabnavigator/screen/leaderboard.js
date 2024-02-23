633ea5bdc4203808243359db08a6e6e2055b72fd
Khue Dao <you@example.com>
2024-02-23 23:43:14 +0700

    back up

--- assets/tabnavigator/screen/leaderboard.js
+++ assets/tabnavigator/screen/leaderboard.js
@@ -1,10 +1,57 @@
-import React from 'react'
-import { Text, Layout } from '@ui-kitten/components'
+import React, { Component } from 'react'
+import { Image, View, TouchableOpacity } from 'react-native'
+import { Text, Layout, Avatar, withStyles, List } from '@ui-kitten/components'
 
-const Leaderboard = () => (
-  <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Leaderboard</Text>
-  </Layout>
+const DATA = [
+  {
+    id: 1,
+    username: 'Đào Minh Dưa',
+    avatarURI:
+      '../../assets/dua.jpg',
+    score: '9.8'
+    },
+  {
+    id: 2,
+    username: 'La Sắc Mầm',
+    avatarURI:
+      '../../assets/mam.jpg',
+    score: '7.0'    } //số store dạng float int g đó thì convert về nhé meo
+]
+
+function Leaderboard() {
+  return (
+    <List
+      style={this.props.themedStyle.container}
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={DATA.id}
+    />
+  )
+}
+
+const renderItem = ({ item }) => (
+  <View style={this.props.themedStyle.card}>
+    
+    <View style={this.props.themedStyle.cardHeader}>
+      <Text category="s1" style={this.props.themedStyle.cardTitle}>
+        {item.username}
+      </Text>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate('Profile')}
+      >
+        <Avatar
+          source={{ uri: item.avatarURI }}
+          size="small"
+          style={this.props.themedStyle.cardAvatar}
+        />
+      </TouchableOpacity>
+    </View>
+    <View style={this.props.themedStyle.cardContent}>
+      <Text category="p2">{item.score}</Text>
+    </View>
+  </View>
 )
+
+
 
 export default Leaderboard
