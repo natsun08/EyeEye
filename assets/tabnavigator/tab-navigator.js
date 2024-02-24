614bdfdb7489f25f75871563ea06c9985233ba91
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-02-25 00:04:56 +0700

    maps
    hai mei you Google API ne~

--- assets/tabnavigator/tab-navigator.js
+++ assets/tabnavigator/tab-navigator.js
@@ -9,12 +9,14 @@ import Setting from './screen/setting';
 
 const Tab = createBottomTabNavigator()
 
+
 function MyTabs() {
   return (
     <Tab.Navigator
       initialRouteName="Dashboard"
       screenOptions={{
         tabBarActiveTintColor: '#e91e63',
+        headerShown:false
       }}
     >
       <Tab.Screen
