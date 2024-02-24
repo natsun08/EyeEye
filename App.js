8ff1dbb2ea5cdccbe65e7d82537d51f3f81943d5
Lê Thị Hà Chi <amuchan134@gmail.com>
2024-02-25 03:12:51 +0700

    MMGHPCK

--- App.js
+++ App.js
@@ -6,27 +6,6 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import StackNav from './assets/stackNavigator/stackNavigator';
 
-// import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-// import { EvaIconsPack } from '@ui-kitten/eva-icons';
-// import StackNav from './assets/stackNavigator/stackNavigator';
-
-// const Stack = createNativeStackNavigator();
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <>
-//         <IconRegistry icons={EvaIconsPack} />
-//         <ApplicationProvider {...eva} theme={eva.light}>
-//           <StackNav />
-//         </ApplicationProvider>
-//       </>
-//     </NavigationContainer>
-//   );
-// }
-
-<<<<<<< Updated upstream
-=======
 export default function App() {
   return (
     <NavigationContainer>
