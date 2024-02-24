6f9efe95c71c7938a777dbf24a3f20ca0e698f48
Lê Thị Hà Chi <amuchan134@gmail.com>
2024-02-25 02:45:51 +0700

    DMGHN

--- assets/stackNavigator/stackNavigator.js
+++ assets/stackNavigator/stackNavigator.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import Login from '../login/login'
 import Regist from '../regist/registration';
 import MyTabs from '../tabnavigator/tab-navigator';
