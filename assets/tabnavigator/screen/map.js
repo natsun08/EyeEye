e14849571fe93f6ca796bbef6ccdbfcda30ce63d
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-02-25 02:28:27 +0700

    add maps
    The Holy Map

--- assets/tabnavigator/screen/map.js
+++ assets/tabnavigator/screen/map.js
@@ -1,66 +1,3 @@
-// import React from 'react'
-// import { Text, Layout } from '@ui-kitten/components'
-// import { Image, FlatList, StyleSheet, View } from 'react-native'
-// import MapView , { Marker } from 'react-native-maps';
-// import React, {useState} from 'react';
-
-// const [mapLat, setMapLat] = useState(6.841776681);
-// const [mapLong, setMapLong] = useState(79.869319);
-
-
-// const Map = () => (
-//   <View style={styles.container}>
-//       <MapView
-//         style={styles.map}
-//         initialRegion={{
-//           latitude: mapLat,
-//           longitude: mapLong,
-//           latitudeDelta: 0.0922,
-//           longitudeDelta: 0.0421,
-//         }}
-//       >
-//       </MapView>
-// </View>
-// //   <>
-// //   <Layout>
-// //     {/* <Image
-// //           style={{ position: 'absolute', top: -14.67, left: -37.94
-// //           }}
-// //           source={require('../../../assets/map-head.png')}
-// //       /> */}
-// //     <MapView
-// //   initialRegion={{
-// //     latitude: 37.78825,
-// //     longitude: -122.4324,
-// //     latitudeDelta: 0.0922,
-// //     longitudeDelta: 0.0421,
-// //   }}
-// // />
-// //     {/* <FlatList
-// //       data={mockData}
-// //       keyExtractor={item => item.id}
-// //       renderItem={({ item }) => (
-// //         <Text style={{ fontSize: 22 }}>
-// //           {item.id} - {item.text}
-// //         </Text>
-// //       )}
-// //     /> */}
-// //   </Layout>
-// //   </>
-// // )
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   map: {
-//     width: '100%',
-//     height: '100%',
-//   },
-// });
-
-// export default Map
-
 import React, {useState} from 'react';
 import MapView , {  Marker,Heatmap  } from 'react-native-maps';
 import { StyleSheet, View, Text, Image } from 'react-native';
@@ -73,7 +10,7 @@ export default function App() {
   return (
     <><View>
       <Image
-          style={{ position: 'absolute', top: -14.67, left: -37.94          }}
+          style={{ position: 'absolute', top: -14.67, left: -37.94}}
           source={require('../../../assets/map-head.png')}
       />
     </View>
